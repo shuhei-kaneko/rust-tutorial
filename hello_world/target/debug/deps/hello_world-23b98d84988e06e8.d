4e44fae8cc7cc144204d4e1ef0ae7f2b6bbf3f01
shuhei-kaneko <shihei-kaneko@tokubai.co.jp>
2022-11-12 22:23:37 +0900

    Initial-commit

---
+++ hello_world/target/debug/deps/hello_world-23b98d84988e06e8.d
@@ -0,0 +1,5 @@
+/Users/shuhei-kaneko/trial/rust_tutorial/hello_world/target/debug/deps/hello_world-23b98d84988e06e8: src/main.rs
+
+/Users/shuhei-kaneko/trial/rust_tutorial/hello_world/target/debug/deps/hello_world-23b98d84988e06e8.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/shuhei-kaneko/trial/rust_tutorial/hello_world/target/debug/hello_world: /Users/shuhei-kaneko/trial/rust_tutorial/hello_world/src/main.rs
