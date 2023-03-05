1ad788e343daf91387b265c6bd5682ba46182223
shuhei-kaneko <shihei-kaneko@tokubai.co.jp>
2023-03-05 22:42:17 +0900

    Finish chapter_5_1

---
+++ chapter_5_1/src/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    let user1: User = build_user(
+        String::from("someone@example.com"),
+        String::from("someusername123"),
+    );
+
+    println!("{}", user1.email);
+
+    let user2 = User {
+        email: String::from("another@example.com"),
+        username: String::from("anotherusername567"),
+        ..user1
+    };
+
+    println!("{}", user2.active);
+}
+
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
