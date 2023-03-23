ecbf192a29ddae8ca7ff5046929b03dc9f1cfc0c
shuhei-kaneko <shihei-kaneko@tokubai.co.jp>
2023-03-23 19:18:04 +0900

    Finish chapter_6_2

---
+++ chapter_6_2/src/bin/coin.rs
@@ -0,0 +1,23 @@
+#[derive(Debug)]
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
+fn main() {
+    println!("{:?}", value_in_cents(Coin::Penny));
+}
